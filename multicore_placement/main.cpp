9fcabec1823eefa74661c81e40438eed645b499d
fabiocremona <fabiocremona@gmail.com>
2014-03-06 09:49:32 +0100

    Initial Commit

---
+++ multicore_placement/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  multicore_placement
+//
+//  Created by Fabio Cremona on 06/03/14.
+//  Copyright (c) 2014 Fabio Cremona. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
